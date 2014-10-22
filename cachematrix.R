d85ca9717f2f8e6dea6ccf4a0abcd0b71fe62e28
Bishesh Manandhar <manandhar.bishesh@gmail.com>
2014-10-22 22:51:53 +0545

    Logics have been prepared

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+		
+		m <- NULL
+        
+        set <- function(y=matrix()) {
 
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        
+        setinve <- function(solve) m <<- solve
+        
+        getinve <- function() m
+        
+        list(set = set, get = get,setinve = setinve,getinve = getinve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+		m <- x$getinve()
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+        m <- solve(data, ...)
+        
+        x$setinve(m)
+        
+        m
 }
